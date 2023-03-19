04de658c58dfeddd33e49b08f26fc72da0873263
iPsychonaut <ian.michael.bollinger@gmail.com>
2023-03-19 01:44:14 -0700

    Consolidated Functions and ensured consistency on hundreds, thousands, and millions of bp on sequences

--- TetraBin_Nucleotide_Encoder.py
+++ TetraBin_Nucleotide_Encoder.py
@@ -2,7 +2,7 @@
 """
 Created on Sat Mar 18 11:35:20 2023
 
-@author: theda
+@author: ian.michael.bollinger@gmail.com
 """
 
 import pandas as pd
@@ -17,10 +17,11 @@ import plotly
 import plotly.graph_objects as go
 import plotly.io as pio
 from plotly.offline import plot
+import os
 
 pio.renderers.default='svg'
 
-def compress_to_binary(nucleotide_sequence):
+def compress_to_binary(fasta_file_path):
     """
     Returns the binary sequence of a nucleotide sequence based on a custom encoding scheme.
     
@@ -38,17 +39,63 @@ def compress_to_binary(nucleotide_sequence):
                        'W': '1001', 'B': '1010', 'D': '1011', 'H': '1100',
                        'V': '1101', 'N': '1110'}
     
-    # Convert the nucleotide sequence to binary
-    binary_string = ''.join(encoding_scheme[n] for n in tqdm.tqdm(nucleotide_sequence, desc='Encoding', unit='nucleotides'))
-    binary_sequence = binary_string.encode()
-    return(binary_sequence)
+    # Open the FASTA file
+    fasta_name = fasta_file_path.split('/')[-1]
 
-def decompress_to_nucleotide(compressed_file):
+    compressed_file_path = fasta_file_path.replace('.fasta','.bin').replace('FASTA','BIN')
+
+    # Use Biopython's SeqIO.parse() function to parse the FASTA file
+    records = list(SeqIO.parse(fasta_file_path, "fasta"))
+
+    # Create a pandas dataframe from the SeqIO records
+    fasta_df = pd.DataFrame(columns=["Accession #", "Description", "Nucleotide"])
+    data = []
+    for record in records:
+        fasta_dictionary = {"Accession #": record.id, "Description": record.description, "Nucleotide": str(record.seq)}
+        data.append(fasta_dictionary)
+    fasta_df = pd.concat([fasta_df, pd.DataFrame(data)], ignore_index=True)
+
+    # Iterate over each the dataframe and generate a 
+    for row, sequence_data in fasta_df.iterrows():
+        acc_numb = sequence_data['Accession #']
+        description = sequence_data['Description']
+        input_string = f'>{acc_numb} {description} '
+        nucleotide_sequence = sequence_data['Nucleotide']
+        
+        # Convert the nucleotide sequence to binary
+        compressed_binary_string = ''.join(encoding_scheme[n] for n in tqdm.tqdm(nucleotide_sequence, desc='Encoding', unit=' nucleotides'))
+        compressed_binary_sequence = compressed_binary_string.encode()
+        fasta_df.loc[row, 'Binary'] = compressed_binary_sequence
+
+        # Define the string and binary number sequence
+        encoded_string = input_string.encode()
+        string_len = len(encoded_string)
+        binary_len = len(compressed_binary_sequence)
+        
+        # Use format codes to create a packed binary string
+        packed_data = struct.pack(f"{string_len}s{binary_len}s", encoded_string, compressed_binary_sequence)
+        
+        # Compress the binary sequence
+        compressed_sequence = gzip.compress(packed_data)
+        
+        # Decompress the binary sequence
+        decompressed_sequence = gzip.decompress(compressed_sequence)
+        
+        # Check Differences
+        sequence_diff(packed_data, decompressed_sequence)
+        
+        # Write the compressed data to a binary file
+        with open(compressed_file_path, 'wb') as f:
+            f.write(compressed_sequence)
+            
+    return(fasta_df, compressed_file_path)
+    
+def decompress_to_nucleotide(compressed_file_path):
     """
     Decodes a binary file to a nucleotide sequence using a custom encoding scheme.
     
     Parameters:
-    - compressed_file: str
+    - compressed_file_path: str
         The path of the binary file to decode.
     
     Returns:
@@ -56,32 +103,41 @@ def decompress_to_nucleotide(compressed_file):
         The nucleotide sequence decoded from the binary file.
     """
     # Read the binary file
-    with gzip.open(compressed_file, 'rb') as f:
-        binary_sequence = f.read()
+    with gzip.open(compressed_file_path, 'rb') as f:
+        compressed_binary_sequence = str(f.read()).strip("b'")
         
-        # Define the reverse encoding scheme
-        reverse_encoding_scheme = {'0000': 'T', '0001': 'A', '0010': 'C', '0011': 'G', 
-                                  '0100': 'R', '0101': 'Y', '0110': 'K', '0111': 'M', 
-                                  '1000': 'S', '1001': 'W', '1010': 'B', '1011': 'D',
-                                  '1100': 'H', '1101': 'V', '1110': 'N'}
-    
-        # Detect the encoding of the binary data
-        detected_encoding = chardet.detect(binary_sequence)['encoding']
-        
-        # If the detected encoding is None, raise an error
-        if detected_encoding is None:
-            raise ValueError('Could not detect encoding of binary data.')
-        
-        # Decode the binary data using the detected encoding
-        binary_string = binary_sequence.decode(detected_encoding)
-        
-        # Convert the binary sequence to nucleotides
-        nucleotide_sequence = []
-        for i in tqdm.tqdm(range(0, len(binary_string), 4), desc='Decoding', unit='nucleotides'):
-            nucleotide_sequence.append(reverse_encoding_scheme[binary_string[i:i+4]])
-        
-        decompressed_nucleotide = ''.join(nucleotide_sequence)
-        return decompressed_nucleotide
+    split = compressed_binary_sequence.split(' ')
+
+    description = ''
+
+    for i, item in enumerate(split):
+        if i == 0:
+            acc_numb = item.strip('>')
+        elif i == len(split)-1:
+            decompressed_binary_string = item
+        else:
+            description += f'{item} '
+
+    # Define the reverse encoding scheme
+    reverse_encoding_scheme = {'0000': 'T', '0001': 'A', '0010': 'C', '0011': 'G', 
+                              '0100': 'R', '0101': 'Y', '0110': 'K', '0111': 'M', 
+                              '1000': 'S', '1001': 'W', '1010': 'B', '1011': 'D',
+                              '1100': 'H', '1101': 'V', '1110': 'N'}    
+
+
+    # Convert the binary sequence to nucleotides
+    nucleotide_sequence = []
+    for i in tqdm.tqdm(range(0, len(decompressed_binary_string), 4), desc='Decoding', unit=' tetrads'):
+        nucleotide_sequence.append(reverse_encoding_scheme[decompressed_binary_string[i:i+4]])
+
+    decompressed_nucleotide = ''.join(nucleotide_sequence)
+
+    decompressed_dictionary = {'Accession #': acc_numb,
+                      'Description': description,
+                      'Nucleotide': decompressed_nucleotide,
+                      'Binary': decompressed_binary_string}
+    decompressed_df = pd.DataFrame(decompressed_dictionary, index=[0])
+    return(decompressed_df)
 
 def sequence_diff(seq1, seq2):
     """
@@ -113,110 +169,77 @@ def sequence_diff(seq1, seq2):
     else:
         print(f"Sequences differ at index/indices: {differing_indices}")
 
+def plot_file_size_comparison(fasta_file_path: str, compressed_file_path: str) -> None:
+    """
+    Compare the file size of the original and compressed files, and display the comparison as a bar graph.
+
+    Args:
+        fasta_file_path (str): Path to the original file.
+        compressed_file_path (str): Path to the compressed file.
+
+    Returns:
+        None
+    """
+    # Fasta Name
+    fasta_name = fasta_file_path.split('/')[-1]
+    
+    # get the file sizes in bytes
+    original_size = os.path.getsize(fasta_file_path)
+    compressed_size = os.path.getsize(compressed_file_path)
+
+    percent_difference_size = round((original_size - compressed_size) / original_size, 2) * 100
+
+    # create a bar graph
+    fig = go.Figure(data=[
+        go.Bar(name='Original File', x=['Original File'], y=[original_size], text=[original_size],
+               textfont=dict(size=30)),
+        go.Bar(name='Compressed File', x=['Compressed File'], y=[compressed_size], text=[compressed_size],
+               textfont=dict(size=30))])
+
+    # update the layout of the graph
+    fig.update_layout(
+        title={'text': f'{fasta_name}<br>File Size Comparison',
+               'y': 0.95,
+               'x': 0.5,
+               'xanchor': 'center',
+               'yanchor': 'top',
+               'font': {'size': 35}},
+        yaxis_title={'text': 'File Size (bytes)',
+                     'font': {'size': 35}},
+        xaxis_tickfont={'size': 35},
+        annotations=[
+            dict(text=f'{percent_difference_size}% File Size Reduction',
+                 x=1,
+                 y=(compressed_size * 1.5),
+                 font_size=30,
+                 showarrow=False)],
+        showlegend=False)
+
+    # update the data labels
+    fig.update_traces(textposition='auto')
+
+    # show the graph
+    plot(fig)
+
 ###############################################################################
 #
 #
 #
 ###############################################################################
 
-# Open the FASTA file
+# Get current working directory
+working_directory = os.getcwd()
 
-fasta_file = 'C:/Users/theda/OneDrive/Documents/Python/AY281023.1.fasta'
+# fasta_file_path = f'{working_directory}/FASTA FILES/AY281023.1.fasta'
 
-# fasta_file = 'C:/Users/theda/OneDrive/Documents/Python/NC_000016.10-89913418-89920973.fasta'
+# fasta_file_path = f'{working_directory}/FASTA FILES/NC_000016.10-89913418-89920973.fasta'
 
-# fasta_file = 'C:/Users/theda/OneDrive/Documents/Python/CM039002.1.fasta'
+fasta_file_path = f'{working_directory}/FASTA FILES/CM039002.1.fasta'
 
-compressed_file = fasta_file.replace('.fasta','.bin')
-
-# Use Biopython's SeqIO.parse() function to parse the FASTA file
-records = list(SeqIO.parse(fasta_file, "fasta"))
-
-# Create a pandas dataframe from the SeqIO records
-df = pd.DataFrame(columns=["Accession #", "Description", "Nucleotide"])
-data = []
-for record in records:
-    data.append({"Accession #": record.id, "Description": record.description, "Nucleotide": str(record.seq)})
-df = pd.concat([df, pd.DataFrame(data)], ignore_index=True)
-
-# Print the dataframe
-print(df)
-
-for row, sequence_data in df.iterrows():
-    print(row)
-    acc_numb = sequence_data['Accession #']
-    description = sequence_data['Description']
-    input_string = f'>{acc_numb} {description}'
-    nucleotide_sequence = sequence_data['Nucleotide']
-    binary_sequence = compress_to_binary(nucleotide_sequence)    
-    df.loc[row, 'Binary'] = binary_sequence
-
-    # Define the string and binary number sequence
-    encoded_string = input_string.encode()
-    string_len = len(encoded_string)
-    binary_len = len(binary_sequence)
-    
-    # Use format codes to create a packed binary string
-    packed_data = struct.pack(f"{string_len}s{binary_len}s", encoded_string, binary_sequence)
-    
-    # Compress the binary sequence
-    compressed_sequence = gzip.compress(packed_data)
-    
-    # Check the compressed size
-    compressed_size = len(compressed_sequence)
-    print(f'Compressed size: {compressed_size}')
-    
-    # Decompress the binary sequence
-    decompressed_sequence = gzip.decompress(compressed_sequence)
-    
-    sequence_diff(packed_data, decompressed_sequence)
-    
-    # Write the compressed data to a binary file
-    with open(compressed_file, 'wb') as f:
-        f.write(compressed_sequence)
+fasta_df, compressed_file_path = compress_to_binary(fasta_file_path)
         
-# decompressed_nucleotide = decompress_to_nucleotide(compressed_file)
+decompressed_df = decompress_to_nucleotide(compressed_file_path)
 
-# sequence_diff(nucleotide_sequence, decompressed_nucleotide)
+sequence_diff(fasta_df['Nucleotide'][0], decompressed_df['Nucleotide'][0])
 
-
-# get the file sizes in bytes
-original_size = os.path.getsize(fasta_file)
-compressed_size = os.path.getsize(compressed_file)
-
-percent_difference_size = round((original_size-compressed_size)/original_size, 2) * 100
-
-# create a bar graph
-fig = go.Figure(data=[go.Bar(name='Original File',
-                             x=['Original File'],
-                             y=[original_size],
-                             text=[original_size],
-                             textfont=dict(size=26)),
-                      go.Bar(name='Compressed File',
-                             x=['Compressed File'],
-                            y=[compressed_size],
-                            text=[compressed_size],
-                            textfont=dict(size=26))])
-
-# update the layout of the graph
-fig.update_layout(
-    title={'text': 'File Sizes Comparison',
-            'y': 0.95,
-            'x': 0.5,
-            'xanchor': 'center',
-            'yanchor': 'top',
-            'font': {'size': 30}},
-    yaxis_title={'text': 'File Size (bytes)',
-                 'font': {'size': 20}},
-    annotations=[
-        dict(text=f'{percent_difference_size}% File Size Reduction',
-             x=1,
-             y=(compressed_size*1.5),
-             font_size=32,
-             showarrow=False)])
-
-# update the data labels
-fig.update_traces(textposition='auto')
-
-# show the graph
-plot(fig)
+plot_file_size_comparison(fasta_file_path, compressed_file_path)
